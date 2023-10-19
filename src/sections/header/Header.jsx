cd72d4d755038345444546c0b87347d56d5da7df
Konstantin Artifex <konstantin.pak.it@gmail.com>
2023-10-19 20:09:00 +0600

    React Portfolio: Header section has been completed

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -16,8 +16,8 @@ const Header = () => {
           performant website today!
         </p>
         <div className="header__cta">
-          <a href="#contact">Let's Talk</a>
-          <a href="#portfolio">My Work</a>
+          <a href="#contact" className="btn primary">Let's Talk</a>
+          <a href="#portfolio" className="btn light">My Work</a>
         </div>
         <div className="header__socials">
           {
