8b7f1a4a864f59693e3e0bf99046cb5340452d82
Konstantin Artifex <konstantin.pak.it@gmail.com>
2023-10-20 17:19:17 +0600

    React Portfolio: Services section has been completed

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,8 +1,26 @@
+import data from './data'
 import './services.css';
+import Card from '../../components/Card'
 
 const Services = () => {
   return (
-    <secion id="services">Services</secion>
+    <section id="services">
+      <h2>My Services</h2>
+      <p>I give you the best in all the services below</p>
+      <div className="container services__container">
+        {
+          data.map(item=>(
+            <Card key={item.id} className="service light">
+              <div className="service__icon">{item.icon}</div>
+              <div className="service__details">
+                <h4>{item.title}</h4>
+                  <p>{item.desc}</p>
+              </div>
+            </Card>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
