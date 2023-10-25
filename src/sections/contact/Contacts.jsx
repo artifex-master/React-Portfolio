dcef8233160393be7f2868459d5155b0df9ef967
Konstantin Artifex <konstantin.pak.it@gmail.com>
2023-10-25 19:15:17 +0600

    React Portfolio: Contacts section has been completed

--- src/sections/contact/Contacts.jsx
+++ src/sections/contact/Contacts.jsx
@@ -1,9 +1,19 @@
+import contacts from './data'
 import './contacts.css'
-
 
 const Contacts = () => {
   return (
-    <section id="contacts">Contacts</section>
+    <section id="contacts">
+      <h2>Get in touch</h2>
+      <p>
+        Shoot me a message via any of the links below!
+      </p>
+      <div className="container contact__container">
+        {
+          contacts.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">{contact.icon}</a>)
+        }
+      </div>
+    </section>
   )
 }
 
